722d865bc93f98f3ba61b721f6a72b2a82ed1030
Rory M Flemming <roryflemming33@gmail.com>
2018-01-31 08:48:58 -0800

    aesthetic update & comments

--- shockProc.m
+++ shockProc.m
@@ -1,4 +1,4 @@
-function [shockTimes] = shockProc(lj,ljChan,stimWindow,stimRect)
+function [shockTimes] = shockProc(lj, ljChan, stimWindow)
 %Send TTL pulses to labjack at constrained-random times.
 %   Uniform randomly distributed times between 3 sec and 30 sec. These
 %   shocks are constrained to be at least 1 sec apart, and occurring only
@@ -10,12 +10,12 @@ function [shockTimes] = shockProc(lj,ljChan,stimWindow,stimRect)
 %   stimWindow = handle for the stimulus display screen
 %   stimReect = handle for the stimulus rect (used to center display)
 
-% Get the times
-shockTimes = zeros(2,1); % place holder
+% Generate shock times
+shockTimes = zeros(2, 1); % place holder
 while length(unique(shockTimes)) < length(shockTimes) % catch repeats
     rng('shuffle');
     numShocks = randi(5);
-    shockTimes = 3 + randi(54,[numShocks,1])./2; % generate times according to constraints described above.
+    shockTimes = 3 + randi(54, [numShocks, 1])./2; % generate times according to constraints described above.
     shockTimes = sort(shockTimes); % sort them in ascending order
 end
     
@@ -23,21 +23,23 @@ end
 % times
 
 dispIntro = "In the next 30 seconds you may be shocked multiple times. \n Please keep your eyes focused on the fixation in the center of the screen. \n Press any key to begin.";   
-m
-DrawFormattedText(stimWindow,dispIntro,'center','center','k');
-Pause(10);
 
-nomFrameRate = Screen('NominalFrameRate',stimWindow); % nominal frame rate of stime display
+DrawFormattedText(stimWindow, dispIntro, 'center', 'center', 'k');
+Pause(10); % Might want to use a wait until key press instead so that the person doesn't get checked
 
-presSecs = [sort(repmat(0:.5:30,1,nomFrameRate*2),'descend') 0]; % List of display numbers0
+nomFrameRate = Screen('NominalFrameRate', stimWindow); % nominal frame rate of stime display
+
+presSecs = [sort(repmat(0:.5:30, 1, nomFrameRate*2), 'descend') 0]; % List of possible shock times in 
 shocked = 0; % variable to track number of times shocked so far
+
+% 
 for i = 1:length(presSecs)
     % Convert display number to string
-    numStr = num2str(presSecs(i));
+    numStr = num2str((30 - presSecs(i)));
     
     % Draw the number to screen (if whole number)
-    if mod(presSecs(i),1) == 0 % if whole number
-        DrawFormattedText(stimWindow,numStr,'center','center','k');
+    if mod(presSecs(i), 1) == 0 % if whole number
+        DrawFormattedText(stimWindow, numStr, 'center', 'center', 'k');
     
         % Flip to the screen
         Screen('Flip', stimWindow);
@@ -46,13 +48,12 @@ for i = 1:length(presSecs)
     % If the time matches with the shock times, send TTL pulse
     if shocked < length(shockTimes)
         if numStr == 30 - shockTimes(shocked + 1) % if it's the shock time...
-            lj.toggleFIO(lj,ljChan); % Triggers ttl pulse at ljChan
+            lj.toggleFIO(lj, ljChan); % Triggers ttl pulse at ljChan
             shocked = shocked + 1; % update shock counter
         end   
     end        
 end
 
 WaitSecs(1); 
-sca
+sca % Is this the best way to close out or....
 end
-
